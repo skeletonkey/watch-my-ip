7f9dde15b86a2100c8d913c1cfbeec5f082ee7ee
Erik Tank <shiny.gift6738@fastmail.com>
2024-05-07 19:55:15 -0700

    get IP part working

--- app/ip/main.go
+++ app/ip/main.go
@@ -1,18 +1,55 @@
 package ip
 
 import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+
 	"github.com/skeletonkey/lib-core-go/logger"
 )
 
-func GetIp() string {
+func GetIp() (string, error) {
 	ipConfig := getConfig()
 	log := logger.Get()
 
 	log.Trace().Str("url", ipConfig.Url).Str("regex", ipConfig.Pattern).Msg("checking for ip")
 
-	ip := "1.1.1.1"
+	client := &http.Client{}
 
-	log.Trace().Str("ip", ip).Msg("fount IP address")
+	req, err := http.NewRequest(http.MethodGet, ipConfig.Url, nil)
+	if err != nil {
+		return "", err
+	}
 
-	return ip
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	compiledRegex, err := regexp.Compile(ipConfig.Pattern)
+	if err != nil {
+		return "", err
+	}
+
+	match := compiledRegex.FindStringSubmatch(string(body))
+
+	if match != nil {
+		if len(match) > 2 {
+			log.Error().Str("matches", strings.Join(match, "|")).Msg("multiple matches found - first one used and others ignored")
+		}
+		log.Trace().Str("ip", match[1]).Msg("found IP address")
+		return match[1], nil
+	} else {
+		err = errors.New("did not find pattern in body")
+		log.Error().Bytes("body", body).Str("pattern", ipConfig.Pattern).Msg(err.Error())
+		return "", err
+	}
 }
--- app/main.go
+++ app/main.go
@@ -15,17 +15,19 @@ func main() {
 
 	var myIP string
 	for {
-		curIP := ip.GetIp()
-
-		if myIP == "" { // first run
-			myIP = curIP
-		} else if myIP != curIP {
-			err := pushover.Notify(fmt.Sprintf("IP has been changed from %s to %s", myIP, curIP))
-			if err != nil {
-				log.Error().Err(err).Msg("issue with pushover service")
+		if curIP, err := ip.GetIp(); err != nil {
+			log.Error().Err(err).Msg("error while attempting to get my IP address")
+		} else {
+			if myIP == "" { // first run
+				myIP = curIP
+			} else if myIP != curIP {
+				err := pushover.Notify(fmt.Sprintf("IP has been changed from %s to %s", myIP, curIP))
+				if err != nil {
+					log.Error().Err(err).Msg("issue with pushover service")
+				}
+				log.Trace().Str("oldIP", myIP).Str("newIP", curIP).Msg("updating IP")
+				myIP = curIP
 			}
-			log.Trace().Str("oldIP", myIP).Str("newIP", curIP).Msg("updating IP")
-			myIP = curIP
 		}
 
 		log.Trace().Int("interval", config.Interval).Msg("sleeping for a while")
