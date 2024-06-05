b924fc9a4ca679b8975d2c0e214e9ca12f9c8691
Erik Tank <erik.tank@pluto.tv>
2024-06-04 20:22:20 -0700

    Update app-init.go

--- app-init.go
+++ app-init.go
@@ -5,12 +5,11 @@ package main
 import instanceGen "github.com/skeletonkey/lib-instance-gen-go/app"
 
 func main() {
-	goVersion := "1.22"
 	app := instanceGen.NewApp("watch-my-ip", "app")
 	app.SetupApp(
 		app.WithConfig(),
 		app.WithGithubWorkflows("linter", "test"),
-		app.WithGoVersion(goVersion),
+		app.WithGoVersion("1.22"),
 		app.WithMakefile(),
 		app.WithPackages("ip"),
 	).Generate()
